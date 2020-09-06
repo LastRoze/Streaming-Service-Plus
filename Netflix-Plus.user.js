4a8004604aec1ffc291b90dcd93061b9bbb1c45d
Yoga Budiman <LastRoze@users.noreply.github.com>
2020-09-06 20:06:21 +0700

    Create Netflix-Plus.user.js

---
+++ Netflix-Plus.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        DNA Netflix Plus
+// @namespace   DNA Netflix Plus
+// @version	1.0
+// @author	Last Roze
+// @description	Dominion With Domination
+// @copyright	©2020 - Yoga Budiman
+// @homepage	https://github.com/LastRoze/
+// @homepageURL	https://github.com/LastRoze/
+// @website	https://lastroze.github.io/
+// @source	https://github.com/LastRoze/Netflix-Plus
+// @icon	https://github.com/LastRoze/Netflix-Plus/blob/master/DNA.jpg?raw=true
+// @iconURL	https://github.com/LastRoze/Netflix-Plus/blob/master/DNA.jpg?raw=true
+// @defaulticon	https://github.com/LastRoze/Netflix-Plus/blob/master/DNA.jpg?raw=true
+// @icon64	https://github.com/LastRoze/Netflix-Plus/blob/master/DNA.jpg?raw=true
+// @icon64URL	https://github.com/LastRoze/Netflix-Plus/blob/master/DNA.jpg?raw=true
+// @updateURL	https://github.com/LastRoze/Netflix-Plus/raw/master/Netflix-Plus.meta.js
+// @downloadURL	https://github.com/LastRoze/Netflix-Plus/raw/master/Netflix-Plus.user.js
+// @supportURL	https://lastroze.github.io/
+// @match	*://*.netflix.com/*
+// @run-at	document-start
+// @grant	none
+// ==/UserScript==
