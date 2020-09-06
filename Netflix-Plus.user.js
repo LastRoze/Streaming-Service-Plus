1a23d304454514e858f30ba12e226d39b2a911bb
Yoga Budiman <LastRoze@users.noreply.github.com>
2020-09-06 20:06:33 +0700

    Update Netflix-Plus.user.js

--- Netflix-Plus.user.js
+++ Netflix-Plus.user.js
@@ -21,3 +21,24 @@
 // @run-at	document-start
 // @grant	none
 // ==/UserScript==
+
+(function() {
+	var DNA = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+			if(mutation.addedNodes.length > 0 && mutation.addedNodes[0].className && mutation.addedNodes[0].className.toString().match(/skip-credits/)) {
+				var iD1 = $(".skip-credits");
+				if (iD1.length > 0) {
+					var bT1 = iD1.find(".nf-flat-button-text");
+					bT1.click();
+                } else if (mutation.addedNodes.length > 0 && mutation.addedNodes[0].className && mutation.addedNodes[0].className.toString().match(/nfa-pos-abs/)) {
+					var iD2 = $(".next-episode-seamless-button");
+					if (iD2.length > 0) {
+						var bT2 = iD2.find(".ltr-14hip7q");
+						bT2.click();
+					}
+				}
+			}
+        });
+    });
+    DNA.observe(document.querySelector('body'), { childList: true, subtree: true });
+})();
