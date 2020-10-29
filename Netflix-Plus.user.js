2a986e1942100c8f9ac2ec85bdfe8a70263dbdcd
Yoga Budiman <LastRoze@users.noreply.github.com>
2020-10-30 06:30:19 +0700

    Update Netflix-Plus.user.js

--- Netflix-Plus.user.js
+++ Netflix-Plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		DNA Netflix Plus
 // @namespace	DNA Netflix Plus
-// @version		1.1
+// @version		2.0
 // @author		Last Roze
 // @description	Dominion With Domination
 // @copyright	©2020 - Yoga Budiman
@@ -23,21 +23,30 @@
 // ==/UserScript==
 
 (function() { 'use strict';
-	var DNA = new MutationObserver(function(mutations) {
+	var DNA1 = new MutationObserver(function(mutations) {
 		mutations.forEach(function(mutation) {
 			if(mutation.addedNodes.length > 0 && mutation.addedNodes[0].className && mutation.addedNodes[0].className.toString().match(/skip-credits/)) {
 				var iD1 = $(".skip-credits");
 				if (iD1.length > 0) {
 					var bT1 = iD1.find(".nf-flat-button-text");
 					bT1.click();
-				} else if (mutation.addedNodes.length > 0 && mutation.addedNodes[0].className && mutation.addedNodes[0].className.toString().match(/main-hitzone-element-container/)) {
-					var iD2 = document.querySelector('.button-nfplayerNextEpisode');
-					if (iD2.length > 0) {
-						bT2.click();
-					}
 				}
 			}
 		});
 	});
-DNA.observe(document.querySelector('body'), { childList: true, subtree: true });
+DNA1.observe(document.querySelector('body'), { childList: true, subtree: true });
+})();
+
+(function() {'use strict';
+    var DNA2 = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if(mutation.addedNodes.length > 0 && mutation.addedNodes[0].className && mutation.addedNodes[0].className.toString().match(/main-hitzone-element-container/)) {
+                var bT2 = document.querySelector('.button-nfplayerNextEpisode')
+                setTimeout(function() {
+                    bT2.click();
+                }, 1);
+            }
+        });
+    });
+DNA2.observe(document.querySelector('body'), { childList: true, subtree: true });
 })();
