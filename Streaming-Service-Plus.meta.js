2271b8fb199686c2f5c723886805e4ec5e711254
Yoga Budiman <LastRoze@users.noreply.github.com>
2021-11-20 04:33:36 +0800

    Update and rename Netflix-Plus.meta.js to Streaming-Service-Plus.meta.js

---
+++ Streaming-Service-Plus.meta.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name		DNA Streaming Service Plus
+// @namespace	DNA Streaming Service Plus
+// @version		1.0
+// @author		Last Roze
+// @description	Dominion With Domination
+// @copyright	©2020 - Yoga Budiman
+// @homepage	https://github.com/LastRoze/
+// @homepageURL	https://github.com/LastRoze/
+// @website		https://lastroze.github.io/
+// @source		https://github.com/LastRoze/Streaming-Service-Plus
+// @icon		https://github.com/LastRoze/Streaming-Service-Plus/blob/master/DNA.jpg?raw=true
+// @iconURL		https://github.com/LastRoze/Streaming-Service-Plus/blob/master/DNA.jpg?raw=true
+// @defaulticon	https://github.com/LastRoze/Streaming-Service-Plus/blob/master/DNA.jpg?raw=true
+// @icon64		https://github.com/LastRoze/Streaming-Service-Plus/blob/master/DNA.jpg?raw=true
+// @icon64URL	https://github.com/LastRoze/Streaming-Service-Plus/blob/master/DNA.jpg?raw=true
+// @updateURL	https://github.com/LastRoze/Streaming-Service-Plus/raw/master/Netflix-Plus.meta.js
+// @downloadURL	https://github.com/LastRoze/Streaming-Service-Plus/raw/master/Netflix-Plus.user.js
+// @supportURL	https://lastroze.github.io/
+// @match		*://*.amazon.ae/*
+// @match		*://*.amazon.ca/*
+// @match		*://*.amazon.cn/*
+// @match		*://*.amazon.co.jp/*
+// @match		*://*.amazon.co.uk/*
+// @match		*://*.amazon.com/*
+// @match		*://*.amazon.com.au/*
+// @match		*://*.amazon.com.br/*
+// @match		*://*.amazon.com.mx/*
+// @match		*://*.amazon.de/*
+// @match		*:/*.amazon.eg/*
+// @match		*://*.amazon.es/*
+// @match		*://*.amazon.fr/*
+// @match		*://*.amazon.in/*
+// @match		*://*.amazon.it/*
+// @match		*://*.amazon.nl/*
+// @match		*://*.amazon.pl/*
+// @match		*://*.amazon.sa/*
+// @match		*://*.amazon.se/*
+// @match		*://*.amazon.sg/*
+// @match		*://*.amazon.tr/*
+// @match		*://*.disneyplus.com/*
+// @match		*://*.hulu.com/*
+// @match		*://*.netflix.com/*
+// @match		*://*.primevideo.com/*
+// @require		https://github.com/LastRoze/DNA-Shield/raw/master/DNA-GM-Config.js
+// @grant		GM_registerMenuCommand
+// @grant		GM_unregisterMenuCommand
+// @grant		GM_setValue
+// @grant		GM_getValue
+// @grant		GM_deleteValue
+// @grant		GM_listValues
+// @grant		GM_openInTab
+// @grant		GM.setValue
+// @grant		GM.getValue
+// @grant		GM.deleteValue
+// @grant		GM.listValues
+// @grant		GM.openInTab
+// ==/UserScript==
